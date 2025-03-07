b26906476d50fcc652e5c779e676eedb0e7eaaf5
Sergio Herrera <sherrera725@gmail.com>
2025-03-07 00:01:24 -0800

    Initial Commit

---
+++ Travo/TravoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravoApp.swift
+//  Travo
+//
+//  Created by Sergio Herrera on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TravoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
